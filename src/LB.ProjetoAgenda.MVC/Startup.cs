e365690850d2d4825ffa560e764aefd2e6f4f702
leonardo.bastos <leonardobastos04@gmail.com>
2021-01-10 17:12:41 -0300

    Criação do Projeto WebMvc

---
+++ src/LB.ProjetoAgenda.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LB.ProjetoAgenda.MVC.Startup))]
+namespace LB.ProjetoAgenda.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
